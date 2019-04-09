378c6984371bf9e7cef8321826d5465f79137023
Neto Chaves <netobac1@gmail.com>
2019-04-09 11:57:52 -0300

    Sending the project architecture

---
+++ src/Screens/Conversas/conversas.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react"
+
+import { View } from "react-native"
+
+// import { Container } from './styles';
+
+export default class Conversas extends PureComponent {
+  state = {}
+
+  render() {
+    return <View />
+  }
+}
