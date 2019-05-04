c764ff77b28c331ba1487ce43b3266bafa59ab6b
Neto Chaves <netobac1@gmail.com>
2019-05-04 15:57:49 -0300

    list contacts of phone

--- src/Screens/Contacts/contacts.js
+++ src/Screens/Contacts/contacts.js
@@ -1,61 +1,85 @@
-import React, { Component } from "react"
+import React, { Component, PureComponent } from "react"
 import { View, FlatList, StyleSheet } from "react-native"
 import { ListItem } from "react-native-elements"
+import Contacts from "react-native-contacts"
+import AsyncStorage from "@react-native-community/async-storage"
 
 import ContactHeader from "../../Components/Contacts/contactHeader"
 
-export default class Contatos extends Component {
-  state = {
-    contacts: [],
-    isLoading: false,
-    isRefreshing: false
+export default class Contatos extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contacts: [],
+      isRefreshing: false
+    }
   }
 
   componentDidMount() {
-    this.loadContacts()
+    AsyncStorage.getItem("@contacts").then(contacts => {
+      console.log("[DID MOUNT IS CALLING]")
+      this.setState({ contacts: JSON.parse(contacts) })
+    })
+  }
+
+  storeData = async contacts => {
+    try {
+      await AsyncStorage.setItem("@contacts", JSON.stringify(contacts))
+    } catch (err) {
+      throw err
+    }
+  }
+
+  getData = async () => {}
+
+  syncronize = () => {
+    Contacts.getAll((err, contacts) => {
+      if (err === "denied") {
+        // error
+      } else {
+        this.storeData(contacts)
+      }
+    })
+    this.getData()
   }
 
   // Nessa função basicamente vamos verificar se existe um contato novo no firebase (movimento de arrastar pra baixo e soltar)
   handleRefresh = () => {
-    const { isRefreshing } = this.state
-    this.setState(
-      {
-        isRefreshing: true
-      },
-      () => {
-        this.loadContacts()
-      }
-    )
+    this.setState({
+      isRefreshing: true
+    })
   }
-
-  // Nessa função é os contatos serão carregados devidamente
-  loadContacts = () => {
-    const { contacts, isLoading } = this.state
-    this.setState({ isLoading: true })
-  }
-
-  onChangeHandler = () => {}
 
   render() {
     const { contacts, isRefreshing } = this.state
+    console.log(this.state.contacts)
 
     return (
       <View style={styles.container}>
         <ContactHeader />
         <FlatList
           data={contacts}
-          renderItem={({ item }) => (
-            <ListItem
-              style={styles.contact}
-              title={item.name.first}
-              subtitle={item.phone}
-              leftAvatar={{
-                source: { uri: item.picture.thumbnail },
-                size: "medium"
-              }}
-            />
-          )}
-          keyExtractor={i => i.email}
+          renderItem={({ item }) => {
+            console.log(item)
+            return (
+              <ListItem
+                style={styles.contact}
+                title={item.givenName}
+                subtitle={
+                  item.phoneNumbers.length > 0
+                    ? item.phoneNumbers[0].number
+                    : null
+                }
+                leftAvatar={{
+                  source: {
+                    uri: item.thumbnailPath === "" ? null : item.thumbnailPath
+                  },
+                  size: "medium"
+                }}
+              />
+            )
+          }}
+          keyExtractor={i => i.recordID}
           refreshing={isRefreshing}
           onRefresh={this.handleRefresh}
           onEndReached={this.handleLoadMore}
