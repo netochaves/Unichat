b4e9e97c147753e73e1ab816b9a03da124d5acd0
Sosolidkk <pedrochaveslimas3@gmail.com>
2019-05-13 18:59:16 -0300

    Configurado routes para tela de configuração e criada o arquivo da tela

---
+++ src/Screens/Config/config.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native"
+
+export default class Config extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <View>
+      </ View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+})
