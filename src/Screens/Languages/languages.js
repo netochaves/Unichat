f16d5b977f347f64a0a65d01696af07ba88fba65
Sosolidkk <pedrochaveslimas3@gmail.com>
2019-05-25 17:43:23 -0300

    Adicionado IDs para cada linguagem, configuração inicial da tela de idiomas feita.

--- src/Screens/Languages/languages.js
+++ src/Screens/Languages/languages.js
@@ -1,13 +1,74 @@
 import React, { Component } from "react"
-import { View } from "react-native"
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native"
+import firebase from "react-native-firebase"
+import data from "~/assets/languages/languages.json"
 
 export default class Languages extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      dataSource: [],
+      currentLanguageCode: ""
+    }
+
+    this.ref = firebase
+      .firestore()
+      .collection("users")
+      .doc(firebase.auth().currentUser.uid)
+  }
+
+  componentDidMount() {
+    this.setState({
+      dataSource: data
+    })
+
+    this.ref.get().then(doc => {
+      this.setState({
+        currentLanguageCode: doc.data().language_code
+      })
+    })
   }
 
   render() {
-    return <View />
+    const { currentLanguageCode, dataSource } = this.state
+    return (
+      <View>
+        <View>
+          <Text style={styles.headerText}>Selecione um idioma</Text>
+        </View>
+        <View>
+          <Text style={styles.headerText}>Idioma atual</Text>
+          <Text style={styles.itemText}>{currentLanguageCode}</Text>
+        </View>
+        <View>
+          <FlatList
+            data={dataSource}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => {
+              return (
+                <TouchableOpacity>
+                  <Text style={styles.itemText}>{item.name}</Text>
+                </TouchableOpacity>
+              )
+            }}
+          />
+        </View>
+      </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  itemText: {
+    fontSize: 18
+  },
+  headerText: {
+    fontSize: 24
+  }
+})
